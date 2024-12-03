dba551f6f5c77ebabebacf5d0a4f72ba9e21ff2f
juaevpa <juaevpa@gmail.com>
2024-12-03 17:41:06 +0100

    Fix Puppeteer configuration for Render deployment

--- postinstall.js
+++ postinstall.js
@@ -1,1 +1,9 @@
- 
+const { execSync } = require("child_process");
+
+try {
+  console.log("Installing Chrome for Puppeteer...");
+  execSync("npx puppeteer browsers install chrome", { stdio: "inherit" });
+} catch (error) {
+  console.error("Error installing Chrome:", error);
+  // Continuar incluso si hay un error
+}
